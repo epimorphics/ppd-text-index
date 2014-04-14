f31b8518cbb3e40c9199cdeab17252eff8377fb5
Brian McBride <brian@epimorphics.com>
2014-04-14 11:17:01 +0100

    Comitting first version - uses large internal cache

---
+++ src/main/java/jena/textindexer2.java
@@ -0,0 +1,112 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jena ;
+
+import org.apache.jena.query.text.DatasetGraphText;
+import org.apache.jena.query.text.EntityDefinition;
+import org.apache.jena.query.text.TextDatasetFactory;
+import org.apache.jena.query.text.TextIndex;
+import org.apache.jena.query.text2.TextIndexer;
+import org.apache.jena.query.text.TextQuery;
+
+import arq.cmd.CmdException;
+import arq.cmdline.ArgDecl;
+import arq.cmdline.CmdARQ;
+
+import com.hp.hpl.jena.query.Dataset;
+
+/**
+ * Text indexer application that will read a dataset and index its triples in
+ * its text index.
+ */
+public class textindexer2 extends CmdARQ {
+
+//    private static Logger      log          = LoggerFactory.getLogger(textindexer.class) ;
+
+    public static final ArgDecl assemblerDescDecl = new ArgDecl(ArgDecl.HasValue, "desc", "dataset") ;
+    
+    protected DatasetGraphText dataset      = null ;
+    protected TextIndex        textIndex    = null ;
+    protected EntityDefinition entityDefinition ;
+
+    static public void main(String... argv) {
+    	System.out.println("********** this is a new indexer ***********");
+        TextQuery.init() ;
+        new textindexer2(argv).mainRun() ;
+    }
+
+    static public void testMain(String... argv) {
+        new textindexer2(argv).mainMethod() ;
+    }
+
+    protected textindexer2(String[] argv) {
+        super(argv) ;
+        super.add(assemblerDescDecl, "--desc=", "Assembler description file") ;
+    }
+
+    @Override
+    protected void processModulesAndArgs() {
+        super.processModulesAndArgs() ;
+        // Two forms : with and without arg.
+        // Maximises similarity with other tools.
+        String file ;
+        
+        if ( ! super.contains(assemblerDescDecl) && getNumPositional() == 0 )
+            throw new CmdException("No assembler description given") ;
+        
+        if ( super.contains(assemblerDescDecl) ) {
+            if ( getValues(assemblerDescDecl).size() != 1 )
+                throw new CmdException("Multiple assembler descriptions given via --desc") ;
+            if ( getPositional().size() != 0 )
+                throw new CmdException("Additional assembler descriptions given") ; 
+            file = getValue(assemblerDescDecl) ;
+        } else {
+            if ( getNumPositional() != 1 )
+                throw new CmdException("Multiple assembler descriptions given as positional arguments") ;
+            file = getPositionalArg(0) ;
+        }
+        
+        if (file == null)
+            throw new CmdException("No dataset specified") ;
+        // Assumes a single test dataset description in the assembler file.
+        Dataset ds = TextDatasetFactory.create(file) ;
+        if (ds == null)
+            throw new CmdException("No dataset description found") ;
+        // get index.
+        dataset = (DatasetGraphText)(ds.asDatasetGraph()) ;
+        textIndex = dataset.getTextIndex() ;
+        if (textIndex == null)
+            throw new CmdException("Dataset has no text index") ;
+        entityDefinition = textIndex.getDocDef() ;
+    }
+
+    @Override
+    protected String getSummary() {
+        return getCommandName() + " assemblerFile" ;
+    }
+
+    @Override
+    protected void exec() {
+    	TextIndexer indexer = 
+    			new TextIndexer(dataset);
+    	TextIndexer.ProgressMonitor pm = new TextIndexer.ProgressMonitor("properties indexed");
+    	indexer.index(pm);
+    	pm.close();
+    }
+}
---
+++ src/main/java/org/apache/jena/query/text2/TextIndexer.java
@@ -0,0 +1,212 @@
+package org.apache.jena.query.text2;
+
+import java.nio.ByteBuffer;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.jena.query.text.DatasetGraphText;
+import org.apache.jena.query.text.Entity;
+import org.apache.jena.query.text.EntityDefinition;
+import org.apache.jena.query.text.TextIndex;
+import org.apache.jena.query.text.TextIndexException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.hp.hpl.jena.graph.Node;
+import com.hp.hpl.jena.query.Dataset;
+import com.hp.hpl.jena.query.DatasetFactory;
+import com.hp.hpl.jena.sparql.core.Quad;
+
+public class TextIndexer {
+	
+	private static Logger      log          = LoggerFactory.getLogger(TextIndexer.class) ;
+	
+	protected Dataset          dataset      = null ;
+	protected DatasetGraphText datasetGraph = null ;
+    protected TextIndex        textIndex    = null ;
+    protected EntityDefinition entityDefinition ;
+	
+    public TextIndexer(Dataset dataset) {
+		this.dataset = dataset;
+		this.datasetGraph = (DatasetGraphText) dataset.asDatasetGraph() ;
+		this.textIndex = datasetGraph.getTextIndex() ;
+	        if (textIndex == null)
+	            throw new TextIndexException("Dataset has no text index") ;
+	    entityDefinition = textIndex.getDocDef() ;
+	}
+    
+    public TextIndexer(DatasetGraphText datasetGraph) {
+		this.datasetGraph = datasetGraph;
+		this.dataset = DatasetFactory.create(datasetGraph);
+		this.textIndex = datasetGraph.getTextIndex() ;
+	    entityDefinition = textIndex.getDocDef() ;
+	}
+	
+	public void index(ProgressMonitor progressMonitor) {	
+		
+        textIndex.startIndexing() ;
+
+        // this is a bit crude and does not scale
+        // there may be smarter structures
+        // could throw some or all of the contents away if we are running out of memory
+        //   that would result in multiple documents in the text index but would not be 'wrong'
+        // 
+        Set<GSPair> processed = new HashSet<GSPair>();
+                
+        Iterator<Quad> quadIter = datasetGraph.find(Node.ANY, Node.ANY, Node.ANY, Node.ANY) ;
+        for ( ; quadIter.hasNext() ; ) {
+            Quad quad = quadIter.next() ;
+            GSPair gs = new GSPair(quad.getGraph(), quad.getSubject());
+            if (processed.contains(gs))  continue ;  // already done this one.
+            processed.add(gs);
+            Entity entity = TextQueryFuncs.entity(entityDefinition, quad.getGraph(), quad.getSubject()) ;
+            int count = addFieldsToEntity(entity, quad.getGraph(), quad.getSubject());
+            if (count > 0) {
+                textIndex.addEntity(entity) ;
+                if (progressMonitor != null) {
+                    progressMonitor.progressByN(count);                    
+                }
+            }
+        }
+        textIndex.finishIndexing();
+    }
+	
+	// find all the properties of the of the subject in the graph 
+	// that are indexed and add them to the entity
+	
+	private int addFieldsToEntity(Entity entity, Node graph, Node subject) {
+		int count = 0;		
+		Iterator<Quad> iter = datasetGraph.find(graph, subject, null, null);
+		for ( ; iter.hasNext(); ) {
+			Quad quad = iter.next();
+			boolean added = TextQueryFuncs.addPropertyToEntity(entityDefinition, entity, quad.getPredicate(), quad.getObject());
+			if (added)  {
+				count++;
+			}
+		}		
+		return count;	
+	}
+
+    private List<Node> getIndexedProperties() {
+        List<Node> result = new ArrayList<Node>() ;
+        for (String f : entityDefinition.fields()) {
+            for ( Node p : entityDefinition.getPredicates(f) )
+                result.add(p) ;
+        }
+        return result ;
+    }
+    /**
+     * A class consisting of a pair of nodes, one a graph node, the other a subject.
+     * 
+     * The graph node can be null, the subject node can't,
+     *
+     */
+    protected static class GSPair {
+    	
+    	private static MessageDigest  md = null ;
+    	static {
+    		try {
+				md = MessageDigest.getInstance("MD5");
+			} catch (NoSuchAlgorithmException e) {
+				// it won't happen, but if it does it will fail soon anyway
+			}
+    	}
+    	private long l1, l2;
+    	
+    	GSPair(Node graph, Node subject) {
+    		byte[] bytes = md.digest((graph.toString() + subject.toString()).getBytes());
+    		ByteBuffer bb = ByteBuffer.allocate(16);;
+    		bb.put(bytes);
+    		l1 = bb.getLong(0);
+    		l2 = bb.getLong(8);
+    	}
+    	
+    	@Override
+    	public boolean equals(Object object) {
+    		if ( object instanceof GSPair ) {
+    			GSPair gs = (GSPair) object ;
+    			return gs.l1 == l1 && gs.l2 == l2;
+    		} else {
+    			return false;
+    		}
+    	}
+    	
+    	@Override
+    	public int hashCode() {
+    		return (int) l1 & 0xFFFFFFFF;
+    	}
+    	
+    }
+
+    // TDBLoader has a similar progress monitor
+    // Not used here to avoid making ARQ dependent on TDB
+    // So potential to rationalise and put progress monitor in a common
+    // utility class
+     public static class ProgressMonitor {
+        String progressMessage ;
+        long   startTime ;
+        long   progressCount ;
+        long   intervalStartTime ;
+        long   progressAtStartOfInterval ;
+        long   reportingInterval = 10000 ; // milliseconds
+
+        public ProgressMonitor(String progressMessage) {
+            this.progressMessage = progressMessage ;
+            start() ; // in case start not called
+        }
+
+        void start() {
+            startTime = System.currentTimeMillis() ;
+            progressCount = 0L ;
+            startInterval() ;
+        }
+
+        private void startInterval() {
+            intervalStartTime = System.currentTimeMillis() ;
+            progressAtStartOfInterval = progressCount ;
+        }
+
+        void progressByOne() {
+        	progressByN(1);
+        }
+
+
+        void progressByN(int n) {
+            progressCount+= n;
+            long now = System.currentTimeMillis() ;
+            if (reportDue(now)) {
+                report(now) ;
+                startInterval() ;
+            }
+        }
+
+        boolean reportDue(long now) {
+            return now - intervalStartTime >= reportingInterval ;
+        }
+
+        private void report(long now) {
+            long progressThisInterval = progressCount - progressAtStartOfInterval ;
+            long intervalDuration = now - intervalStartTime ;
+            long intervalSeconds = intervalDuration / 1000;
+            if (intervalSeconds == 0) intervalSeconds = 1;
+            long overallDuration = now - startTime ;
+            String message = progressCount + " (" + progressThisInterval / intervalSeconds + " per second) "
+                             + progressMessage + " (" + progressCount / Math.max(overallDuration / 1000, 1)
+                             + " per second overall)" ;
+            log.info(message) ;
+        }
+
+        public void close() {
+            long overallDuration = System.currentTimeMillis() - startTime ;
+            String message = progressCount + " (" + progressCount / Math.max(overallDuration / 1000, 1)
+                             + " per second) " + progressMessage ;
+            log.info(message) ;
+        }
+    }
+
+}
