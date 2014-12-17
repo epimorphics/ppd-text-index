fa8bae466469f5fa636659e69e426c3533a220b5
Ian Dickinson <ian@epimorphics.com>
2014-12-17 17:31:30 +0000

    Initial functional test working - index and query base dataset

--- src/test/java/com/epimorphics/lr/jena/query/text/FunctionalTest.java
+++ src/test/java/com/epimorphics/lr/jena/query/text/FunctionalTest.java
@@ -10,19 +10,20 @@
 package com.epimorphics.lr.jena.query.text;
 
 
-import java.io.File;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 import org.apache.jena.query.text.TextQuery;
 import org.apache.jena.riot.RDFDataMgr;
 import org.junit.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import static org.junit.Assert.*;
-
 import com.hp.hpl.jena.query.*;
-import com.hp.hpl.jena.rdf.model.Model;
-import com.hp.hpl.jena.tdb.TDB;
 
 /**
  * Functional test for test indexing, covering building an index from scratch
@@ -48,63 +49,6 @@ public class FunctionalTest
 
     static final String PPD_BASE_TEST_DATA = "landregistry_sample.nq";
 
-    static final String COMMON_SPARQL_PREFIXES =
-            "prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
-            "prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
-            "prefix owl: <http://www.w3.org/2002/07/owl#>\n" +
-            "prefix xsd: <http://www.w3.org/2001/XMLSchema#>\n" +
-            "prefix sr: <http://data.ordnancesurvey.co.uk/ontology/spatialrelations/>\n" +
-            "prefix lrhpi: <http://landregistry.data.gov.uk/def/hpi/>\n" +
-            "prefix lrppi: <http://landregistry.data.gov.uk/def/ppi/>\n" +
-            "prefix skos: <http://www.w3.org/2004/02/skos/core#>\n" +
-            "prefix lrcommon: <http://landregistry.data.gov.uk/def/common/>\n" +
-            "PREFIX  text: <http://jena.apache.org/text#>\n" +
-            "PREFIX  ppd:  <http://landregistry.data.gov.uk/def/ppi/>\n" +
-            "PREFIX  lrcommon: <http://landregistry.data.gov.uk/def/common/>\n";
-
-    static final String QUERY_TOWN_BRACKNELL =
-            COMMON_SPARQL_PREFIXES +
-            "SELECT  ?item ?ppd_propertyAddress ?ppd_hasTransaction ?ppd_pricePaid ?ppd_publishDate ?ppd_transactionDate ?ppd_transactionId ?ppd_estateType ?ppd_newBuild ?ppd_propertyAddressCounty ?ppd_propertyAddressDistrict ?ppd_propertyAddressLocality ?ppd_propertyAddressPaon ?ppd_propertyAddressPostcode ?ppd_propertyAddressSaon ?ppd_propertyAddressStreet ?ppd_propertyAddressTown ?ppd_propertyType ?ppd_recordStatus\n" +
-            "WHERE\n" +
-            "  { ?ppd_propertyAddress text:query _:b0 .\n" +
-            "    _:b0 <http://www.w3.org/1999/02/22-rdf-syntax-ns#first> lrcommon:town .\n" +
-            "    _:b0 <http://www.w3.org/1999/02/22-rdf-syntax-ns#rest> _:b1 .\n" +
-            "    _:b1 <http://www.w3.org/1999/02/22-rdf-syntax-ns#first> \"( bracknell )\" .\n" +
-            "    _:b1 <http://www.w3.org/1999/02/22-rdf-syntax-ns#rest> _:b2 .\n" +
-            "    _:b2 <http://www.w3.org/1999/02/22-rdf-syntax-ns#first> 3000000 .\n" +
-            "    _:b2 <http://www.w3.org/1999/02/22-rdf-syntax-ns#rest> <http://www.w3.org/1999/02/22-rdf-syntax-ns#nil> .\n" +
-            "    ?item ppd:propertyAddress ?ppd_propertyAddress .\n" +
-            "    ?item ppd:hasTransaction ?ppd_hasTransaction .\n" +
-            "    ?item ppd:pricePaid ?ppd_pricePaid .\n" +
-            "    ?item ppd:publishDate ?ppd_publishDate .\n" +
-            "    ?item ppd:transactionDate ?ppd_transactionDate .\n" +
-            "    ?item ppd:transactionId ?ppd_transactionId\n" +
-            "    OPTIONAL\n" +
-            "      { ?item ppd:estateType ?ppd_estateType }\n" +
-            "    OPTIONAL\n" +
-            "      { ?item ppd:newBuild ?ppd_newBuild }\n" +
-            "    OPTIONAL\n" +
-            "      { ?ppd_propertyAddress lrcommon:county ?ppd_propertyAddressCounty }\n" +
-            "    OPTIONAL\n" +
-            "      { ?ppd_propertyAddress lrcommon:district ?ppd_propertyAddressDistrict }\n" +
-            "    OPTIONAL\n" +
-            "      { ?ppd_propertyAddress lrcommon:locality ?ppd_propertyAddressLocality }\n" +
-            "    OPTIONAL\n" +
-            "      { ?ppd_propertyAddress lrcommon:paon ?ppd_propertyAddressPaon }\n" +
-            "    OPTIONAL\n" +
-            "      { ?ppd_propertyAddress lrcommon:postcode ?ppd_propertyAddressPostcode }\n" +
-            "    OPTIONAL\n" +
-            "      { ?ppd_propertyAddress lrcommon:saon ?ppd_propertyAddressSaon }\n" +
-            "    OPTIONAL\n" +
-            "      { ?ppd_propertyAddress lrcommon:street ?ppd_propertyAddressStreet }\n" +
-            "    OPTIONAL\n" +
-            "      { ?ppd_propertyAddress lrcommon:town ?ppd_propertyAddressTown }\n" +
-            "    OPTIONAL\n" +
-            "      { ?item ppd:propertyType ?ppd_propertyType }\n" +
-            "    OPTIONAL\n" +
-            "      { ?item ppd:recordStatus ?ppd_recordStatus }\n" +
-            "  }\n" +
-            "LIMIT   100\n";
 
     /***********************************/
     /* Static variables                */
@@ -133,20 +77,24 @@ public class FunctionalTest
         TextQuery.init() ;
     }
 
-    /** Reset the TDB dataset each time */
+    /** Reset the TDB dataset each time by deleting and reloading */
     @Before
     public void resetTDB() {
         log.debug( "Reset TDB" );
         File tdbDir = new File( TDB_TEST_ROOT );
         File indexDir = new File( TDB_INDEX_ROOT );
 
-        if (tdbDir.exists()) {
-            log.debug( "Removing existing TDB directory" );
-            tdbDir.delete();
+        try {
+            if (tdbDir.exists()) {
+                FileUtils.deleteDirectory( tdbDir );
+            }
+            if (indexDir.exists()) {
+                FileUtils.deleteDirectory( indexDir );
+            }
         }
-        if (indexDir.exists()) {
-            log.debug( "Removing existing Lucene index directory" );
-            tdbDir.delete();
+        catch (IOException e) {
+            log.error( e.getMessage(), e );
+            throw new RuntimeException( e );
         }
 
         tdbDir.mkdir();
@@ -155,9 +103,12 @@ public class FunctionalTest
         ds = loadDataset( ASSEMBLER_CONFIG, PPD_BASE_TEST_DATA );
     }
 
+    /**
+     * Test that the base dataset is indexed as it is loaded.
+     */
     @Test
     public void testCreateIndexForBaseDataset() {
-        ResultSet results = queryData( ds, QUERY_TOWN_BRACKNELL );
+        ResultSet results = queryData( ds, loadQuery( TEST_RESOURCES + "query-town-dover.sparql" ) );
         assertTrue( "Query failed to return any results", results.hasNext() );
 
         int n = 0;
@@ -167,25 +118,33 @@ public class FunctionalTest
             QuerySolution soln = results.next();
             n++;
 
-            log.debug( "solution " + n + " - postcode " + soln.getLiteral( "ppd_propertyAddressPostcode" ));
-            seenPostcode = seenPostcode || soln.getLiteral( "ppd_propertyAddressPostcode" ).equals( "RG12 0UT" );
+            String postcode = soln.getLiteral( "ppd_propertyAddressPostcode" ).getLexicalForm();
+            seenPostcode = seenPostcode || postcode.equals( "CT17 9LD" );
         }
 
         assertTrue( "Expected to see postcode", seenPostcode );
-        assertEquals( 100, n );
+        assertEquals( 1, n );
     }
 
     /***********************************/
     /* Internal implementation methods */
     /***********************************/
 
+    /**
+     * @return A dataset given an assembler description
+     */
     public Dataset createDsFromAssembler( String assemblerFile ) {
         Dataset ds = DatasetFactory.assemble( TEST_RESOURCES + assemblerFile, TEXT_CONFIG_ROOT );
         return ds ;
     }
 
+    /**
+     * Load a datafile into a newly created dataset, specified by the given assembler file
+     * @param assemblerFile
+     * @param dataFile
+     * @return
+     */
     public Dataset loadDataset( String assemblerFile, String dataFile ) {
-        log.debug( "Load begin" );
         Dataset ds = null;
         try {
             ds = createDsFromAssembler( assemblerFile );
@@ -202,8 +161,6 @@ public class FunctionalTest
             ds.begin( ReadWrite.READ );
             log.debug( "Load completed, dataset size is: " + ds.getNamedModel( "urn:x-arq:UnionGraph" ).size() );
             ds.end();
-
-            TDB.sync( ds );
         }
         catch (Exception e) {
             log.error( e.getMessage() );
@@ -213,10 +170,10 @@ public class FunctionalTest
         return ds;
     }
 
+    /**
+     * @return The resultset from running the given sparql query against the given dataset
+     */
     public ResultSet queryData( Dataset ds, String sparql ) {
-//        String qs = StrUtils.strjoinNL("SELECT * ", " { ?s text:query (rdfs:label 'X1') ;", "      rdfs:label ?label",
-//                                       " }") ;
-
         ds.begin(ReadWrite.READ) ;
         ResultSet rs = null;
 
@@ -231,43 +188,26 @@ public class FunctionalTest
         return rs;
     }
 
+    /**
+     * Load a sparql query from a file
+     * @param queryFile
+     * @return
+     */
+    public String loadQuery( String queryFile ) {
+        try {
+            File file = new File( queryFile );
+            assertTrue( "Query file does not exist: " + queryFile, file.exists() );
 
+            return FileUtils.readFileToString( file );
+        }
+        catch (IOException e) {
+            log.error( e.getMessage(), e );
+            throw new RuntimeException( e );
+        }
+    }
 
     /***********************************/
     /* Inner class definitions         */
     /***********************************/
 
 }
-
-//    @Test
-//    public void buildText_01() {
-//        createAssembler("text-config.ttl") ;
-//    }
-//
-//    @Test
-//    public void buildText_02() {
-//        Dataset ds = createAssembler("text-config-union.ttl") ;
-//        assertTrue(ds.getContext().isDefined(TextQuery.textIndex)) ;
-//        assertTrue(ds.getContext().isDefined(TDB.symUnionDefaultGraph)) ;
-//    }
-//
-//    @Test
-//    public void buildText_03() {
-//        createCode() ;
-//    }
-//
-//    @Test
-//    public void buildText_04() {
-//        Dataset ds = createAssembler("text-config.ttl") ;
-//        loadData(ds) ;
-//        queryData(ds) ;
-//    }
-//
-//    @Test
-//    public void buildText_05() {
-//        Dataset ds = createCode() ;
-//        loadData(ds) ;
-//        queryData(ds) ;
-//    }
-//
-
