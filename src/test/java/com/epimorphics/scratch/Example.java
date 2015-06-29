fb410dca7bf457f9d50d47cf24467c6cf3f44c3c
Chris Dollin <ehog.hedge@googlemail.com>
2015-06-29 12:19:20 +0100

    Corrected name of TDB class to be in org.apache.

---
+++ src/test/java/com/epimorphics/scratch/Example.java
@@ -0,0 +1,35 @@
+package com.epimorphics.scratch;
+
+import java.util.Iterator;
+
+import org.apache.jena.query.text.TextDocProducerTriples;
+import org.apache.jena.query.text.TextIndex;
+
+import org.apache.jena.graph.Node;
+import org.apache.jena.sparql.core.DatasetGraph;
+import org.apache.jena.sparql.core.Quad;
+import org.apache.jena.sparql.core.QuadAction;
+
+public class Example extends TextDocProducerTriples {
+
+	final DatasetGraph dg;
+	
+	public Example(DatasetGraph dg, TextIndex indexer) {
+		super(indexer);
+		this.dg = dg;
+	}
+	
+	public void change(QuadAction qaction, Node g, Node s, Node p, Node o) {
+		if (qaction == QuadAction.ADD) {
+			if (alreadyHasOne(s, p)) super.change(qaction, g, s, p, o);
+		}
+	}
+
+	private boolean alreadyHasOne(Node s, Node p) {
+		int count = 0;
+		Iterator<Quad> quads = dg.find( null, s, p, null );
+		while (quads.hasNext()) { quads.next(); count += 1; }
+		return count > 1;
+	}
+
+}
